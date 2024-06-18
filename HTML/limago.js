cae19d83d0ddb5d159d0459070aed3dff78910f3
KauePO <kaueperes2004@gmail.com>
2024-06-18 15:11:45 -0300

    Finalização Home e Processamento

--- HTML/limago.js
+++ HTML/limago.js
@@ -1,7 +1,6 @@
 function processamento() {
-
-    var descricao = document.getElementById ("descricao")
-    var legendastexto = document.getElementById("legendastexto")
+    var descricao = document.getElementById("descricao");
+    var legendastexto = document.getElementById("legendastexto");
     var librastexto = document.getElementById("librastexto");
     var imagem = document.getElementById("upload");
     var texto = document.getElementById("texto");
@@ -9,9 +8,9 @@ function processamento() {
     var caixa2 = document.getElementById("defaultCheck2");
     var librapro = document.getElementById("librapro");
     var subtexto = document.getElementById("subtexto");
-
     var processamentotexto = document.getElementById("processamentotexto");
-    var btdoacao= document.getElementById("btdoacao");
+    var btdoacao = document.getElementById("btdoacao");
+
     // Ocultar os elementos
     legendastexto.style.display = "none";
     librastexto.style.display = "none";
@@ -21,13 +20,20 @@ function processamento() {
     librapro.style.display = "none";
     descricao.style.display = "none";
     subtexto.style.display = "none";
-    // PROCESSAMENTO
-    imagem.src="img/loading.gif";
-    imagem.id="loading";
 
-    processamentotexto.textContent= "O seu vídeo está sendo processado, por favor aguarde"
-    btdoacao.style.backgroundImage = "url('img/doacao.png')";
-    
+    // PROCESSAMENTO
+    imagem.src = "img/loading.gif";
+    imagem.id = "loading";
+
+    processamentotexto.textContent = "O seu vídeo está sendo processado, por favor aguarde";
+    btdoacao.textContent = "Faça uma doação";
+    btdoacao.style.display = "inline-block";
+
+    // Redirecionar após 4 segundos
+    setTimeout(function() {
+        window.location.href = "download.html"; // Substitua "download.html" pela URL desejada
+    }, 3500); // 4000 milissegundos = 4 segundos
 }
+
 var upload = document.getElementById("upload");
 btupload.addEventListener("click", processamento);
